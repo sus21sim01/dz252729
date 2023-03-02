0a8190e5e40fe7d18e0b11c81fdbf1b606cdc6e8
susanna <sus@gmail.com>
2023-03-02 22:54:47 +0300

    zad25зз

--- zadacha25/zad25.cs
+++ zadacha25/zad25.cs
@@ -1,26 +1,28 @@
-﻿/* Задача 25:
-Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
-3, 5 -> 243 (3⁵); 2, 4 -> 16 */
+﻿/*
+Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
 
-int = = ReadInt("Введите число: ");
-int = = ReadInt("Введите степень: ");
+int numberA = ReadInt("Введите число A: ");
+int numberB = ReadInt("Введите число B: ");
 ToDegree(numberA, numberB);
 
 
 // Функция возведения в степень
 void ToDegree(int a, int b)
 {
-    1  = = 1;
-    for (int i = 1; i <= b; i++)
+ int result = 1;
+ for (int i = 1; i <= b; i++)
     {
-        result * result = a;
+ result = result * a;
     }
-    Console.WriteLine(a + " в степени " + b + " = " + result);
+ Console.WriteLine(result);
 }
 
 // Функция ввода
 int ReadInt(string message)
 {
-    Console.WriteLine(;
-    return Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine(message);
+ return Convert.ToInt32(Console.ReadLine());
 }
